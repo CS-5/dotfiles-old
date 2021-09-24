0088da782d2d9c7656c9049028c1564c33477259
CS-5 <me@carsonseese.com>
2021-09-24 10:27:55 -0400

    Major fish rework

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,55 +1,30 @@
-### THEME CONFIG ###
+if status is-interactive
+    # Setup Starfish
+    starship init fish | source
+end
 
-# Fish syntax highlighting
-set -g fish_color_autosuggestion '555'  'brblack'
-set -g fish_color_cancel -r
-set -g fish_color_command --bold
-set -g fish_color_comment red
-set -g fish_color_cwd green
-set -g fish_color_cwd_root red
-set -g fish_color_end brmagenta
-set -g fish_color_error brred
-set -g fish_color_escape 'bryellow'  '--bold'
-set -g fish_color_history_current --bold
-set -g fish_color_host normal
-set -g fish_color_match --background=brblue
-set -g fish_color_normal normal
-set -g fish_color_operator bryellow
-set -g fish_color_param cyan
-set -g fish_color_quote yellow
-set -g fish_color_redirection brblue
-set -g fish_color_search_match 'bryellow'  '--background=brblack'
-set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
-set -g fish_color_user brgreen
-set -g fish_color_valid_path --underline
+##### Setup Plugins #####
 
-# Manpage Config
-set -g man_blink -o red
-set -g man_bold -o green
-set -g man_standout -b black 93a1a1
-set -g man_underline -u 93a1a1
+fundle plugin joseluisq/gitnow
+fundle plugin jorgebucaran/nvm.fish
+fundle plugin jethrokuan/z
+fundle plugin gazorby/fish-abbreviation-tips
+fundle plugin sharkdp/bat
 
-# Setup Enviorment
+fundle init
 
-## Go
+##### Setup Enviorment #####
+
+fish_add_path $HOME/.local/bin
+
+# Go
 set GOROOT /usr/local/go
 set GOPATH $HOME/programming/go
 
-## Rust
+# Rust
 set CARGOPATH $HOME/.cargo
 
-# Aliases
-alias g='git'
-alias v='vim'
-alias reload='omf reload'
-alias python='python3'
-alias what='apropos'
+##### Misc #####
 
-# Init Jump
-status --is-interactive; and source (jump shell fish | psub)
-
-# Init Starfish
-starship init fish | source
-
-# Clear greeting
+# Clear Greeting
 set fish_greeting
